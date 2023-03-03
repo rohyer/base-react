2efd5237c5b692f7bbe62cc793cfbee056f19847
Rohyer <guilhermerl.dev@gmail.com>
2023-03-02 23:19:23 -0300

    include banner

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from './layouts/Header/Header';
 import Footer from './layouts/Footer/Footer';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Banner from './layouts/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
       <Footer />
     </div>
   );
