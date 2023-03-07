87c23e16c2ac423da01c82bc634b05e91a3d42b8
Rohyer <guilhermerl.dev@gmail.com>
2023-03-06 22:59:37 -0300

    add about section to app

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from './layouts/Footer/Footer';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Banner from './layouts/Banner/Banner';
+import About from './layouts/About/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <About />
       <Footer />
     </div>
   );
