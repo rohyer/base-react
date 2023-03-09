676290b38f498ca4d6fff7909ba981128b7fda50
Rohyer <guilhermerl.dev@gmail.com>
2023-03-08 22:42:51 -0300

    develop services section

---
+++ src/layouts/Services/Services.jsx
@@ -0,0 +1,94 @@
+import './Services.css';
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+
+const headers = { 'Authorization': 'Bearer ' + process.env.REACT_APP_JWT_API_TOKEN}
+
+const News = () => {
+  const [services, setServices] = useState({});
+  const [servicesPosts, setServicesPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:1337/api/paginas/6', {
+        headers
+      });
+      const res = await data.json();
+      setServices(res.data.attributes);
+    }
+
+    const fetchPostsData = async () => {
+      const data = await fetch('http://localhost:1337/api/servicos?populate=*', {
+        headers
+      });
+      const res = await data.json();
+      setServicesPosts(res.data);
+    }
+
+    fetchData();
+    fetchPostsData();
+  }, []);
+
+  console.log(servicesPosts);
+
+  const getLink = () => {
+    const link = services.homeButtonLink;
+
+    if (link) {
+      const buttonTitle = services.homeButtonText;
+      const targetLink = services.homeTab ? '_blank' : '_self';
+
+      return <a href={`${link}`} target={targetLink}>{buttonTitle}</a>
+    }
+  }
+
+  return (
+    <div className="services">
+      <div className="container-md">
+        <div className="top-content">
+          <h2>{services.homeTitle}</h2>
+          <p>{services.homeContent}</p>
+        </div>
+
+        <div className="posts">
+
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            navigation
+            autoplay
+            spaceBetween={15}
+            slidesPerView={4}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            {
+              servicesPosts.map((item, index) => (
+                <SwiperSlide key={index}>
+                  <a key={index} href={item.attributes.link} target={item.attributes.tab ? '_blank' : '_self'} className="post">
+                    <img src={`${process.env.REACT_APP_API_URL}${item.attributes.cardImage.data.attributes.url}`} alt="Imagem" />
+                    <div className="post-content">
+                      <p>{item.attributes.cardTitle}</p>
+                    </div>
+                  </a>
+                </SwiperSlide>
+              ))
+            }
+          </Swiper>
+        </div>
+        
+        <div className="bottom-content">
+          {getLink()}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default News
