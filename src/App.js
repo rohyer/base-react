422d663bca8376edb21f653fe9e5c6515eb10dbb
Rohyer <guilhermerl.dev@gmail.com>
2023-05-07 00:34:32 -0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import './App.css';
-import Header from './layouts/Header/Header';
-import Footer from './layouts/Footer/Footer';
+import Header from './Home/Header/Header';
+import Footer from './Home/Footer/Footer';
+import Home from './Home/Home';
+import Page from './Pages/Page/Page';
+import PostsPage from './Pages/PostsPage/PostsPage';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Banner from './layouts/Banner/Banner';
-import About from './layouts/About/About';
-import News from './layouts/News/News';
-import Partners from './layouts/Partners/Partners';
-import Services from './layouts/Services/Services';
-import Team from './layouts/Team/Team';
-import Cta from './layouts/Cta/Cta';
+
+const headers = {
+  'Authorization': 'Bearer ' + process.env.REACT_APP_TOKEN
+}
 
 function App() {
+  const [routes, setRoutes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('http://localhost:1337/api/paginas?fields[0]=menuTitle&fields[1]=slug&fields[2]=pagePosts', {
+        headers
+      });
+      const res = await data.json();
+      setRoutes(res.data)
+    }
+
+    fetchData();
+  }, []);
+
+  const getPages = () => {
+    const page = routes.filter(route => !route.attributes.pagePosts);
+    console.log(page);
+    return page;
+  }
+
+  const getPagesPosts = (routes) => {
+    const pagesPosts = routes.filter(route => route.attributes.pagePosts);
+    return pagesPosts;
+  }
+
   return (
     <div className="App">
       <Header />
-      <Banner />
-      <About />
-      <News />
-      <Partners />
-      <Services />
-      <Team />
-      <Cta />
+        <Routes>
+          <Route index element={<Home />} />
+
+          {
+            getPagesPosts(routes).map(({ id, attributes }) => (
+              <Route key={id} path={`/${attributes.slug}`} element={<PostsPage id={id} key={id} />} />
+            ))
+          }
+          {
+            getPages(routes).map(({ id, attributes }) => (
+              <Route key={id} path={`/${attributes.slug}`} element={<Page id={id} key={id} />} />
+            ))
+          }
+        </Routes>
       <Footer />
     </div>
   );
---
+++ src/Pages/PostsPage/PostsPage.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+const headers = {
+  'Authorization': 'Bearer ' + process.env.REACT_APP_TOKEN
+}
+
+const PostsPage = ({ id }) => {
+  const [page, setPage] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(`http://localhost:1337/api/paginas/${id}`, {
+        headers
+      });
+      const res = await data.json();
+      setPage(res.data.attributes);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(page);
+
+  return (
+    
+    <div>Página com Postagens {page.innerTitle}</div>
+  )
+}
+
+export default PostsPage
