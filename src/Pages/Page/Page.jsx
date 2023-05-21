9945ea3e311ea2966fe9b71e24a4db13cb109b36
Rohyer <guilhermerl.dev@gmail.com>
2023-05-21 15:44:18 -0300

    develop internal pages

--- src/Pages/Page/Page.jsx
+++ src/Pages/Page/Page.jsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import './Page.css';
+import React, { useEffect, useState } from 'react';
+import { Container, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+
 
 const headers = {
   'Authorization': 'Bearer ' + process.env.REACT_APP_TOKEN
@@ -6,24 +11,40 @@ const headers = {
 
 const Page = ({ id }) => {
   const [page, setPage] = useState({});
+  const [image, setImage] = useState();
+  let navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch(`http://localhost:1337/api/paginas/${id}`, {
+      const data = await fetch(`http://localhost:1337/api/paginas/${id}?populate=*`, {
         headers
       });
       const res = await data.json();
       setPage(res.data.attributes);
+      setImage(res.data.attributes.innerImage.data.attributes.url);
     }
 
     fetchData();
-  }, []);
-
-  console.log(page);
+  }, [id]);
 
   return (
     
-    <div>Página sem Postagens {page.innerTitle}</div>
+    <div className="page">
+      <Container fixed>
+        <h1 class="inner-title--responsive">{page.innerTitle}</h1>
+
+        <img className="inner-img" src={`${process.env.REACT_APP_API_URL}${image}`} alt="Imagem" />
+        <div className="inner-text">
+          <h1 class="inner-title--desktop">{page.innerTitle}</h1>
+          <ReactMarkdown>
+            {page.innerContent}
+          </ReactMarkdown>
+        </div>
+      </Container>
+      <Container fixed>
+        <Button className="back-btn" variant="contained" color="inherit" onClick={() => navigate(-1)}>Voltar</Button>
+      </Container>
+    </div>
   )
 }
 
--- src/Pages/PostsPage/PostsPage.jsx
+++ src/Pages/PostsPage/PostsPage.jsx
@@ -1,4 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import './PostsPage.css';
+import React, { useEffect, useState } from 'react';
+import { Container, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 const headers = {
   'Authorization': 'Bearer ' + process.env.REACT_APP_TOKEN
@@ -6,24 +10,41 @@ const headers = {
 
 const PostsPage = ({ id }) => {
   const [page, setPage] = useState({});
+  const [image, setImage] = useState();
+  let navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch(`http://localhost:1337/api/paginas/${id}`, {
+      const data = await fetch(`http://localhost:1337/api/paginas/${id}?populate=*`, {
         headers
       });
       const res = await data.json();
       setPage(res.data.attributes);
+      setImage(res.data.attributes.innerImage.data.attributes.url);
     }
 
     fetchData();
-  }, []);
+  }, [id]);
 
   console.log(page);
 
   return (
-    
-    <div>Página com Postagens {page.innerTitle}</div>
+    <div className="posts-page">
+      <Container fixed>
+        <h1 class="inner-title--single">{page.innerTitle}</h1>
+
+        <img className="inner-img" src={`${process.env.REACT_APP_API_URL}${image}`} alt="Imagem" />
+        <div className="inner-text">
+          <ReactMarkdown>
+            {page.innerContent}
+          </ReactMarkdown>
+        </div>
+
+      </Container>
+      <Container fixed>
+        <Button className="back-btn" variant="contained" color="inherit" onClick={() => navigate(-1)}>Voltar</Button>
+      </Container>
+    </div>
   )
 }
 
